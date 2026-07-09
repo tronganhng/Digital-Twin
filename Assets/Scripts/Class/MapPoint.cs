73f2bbf6dfe3b1c89b2486183f1dd055023d782c
tronganh <latgoto2004@gmail.com>
2026-07-09 16:58:24 +0700

    update map point

--- Assets/Scripts/Class/MapPoint.cs
+++ Assets/Scripts/Class/MapPoint.cs
@@ -1,9 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class MapPoint : MonoBehaviour
 {
+    [SerializeField] private TextMeshPro label;
     [SerializeField] private string pointName;
 
     public string PointName => pointName;
     public Vector3 Position => transform.position;
+
+    void Start()
+    {
+        label.text = pointName;
+    }
 }
